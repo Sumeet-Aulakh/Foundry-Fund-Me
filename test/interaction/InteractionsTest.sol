6d27350ae38628c7360c366ff67e2af46fa5ca96
Sumeet Singh Aulakh <sumeetbinjal@gmail.com>
2023-12-24 08:38:33 -0600

    Add interaction scripts and test case for FundMe contract

---
+++ test/interaction/InteractionsTest.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+
+contract InteractionsTest is Test {
+    FundMe fundMe;
+
+    address USER = makeAddr("user"); //Fake New Address
+    uint256 constant SEND_VALUE = 0.1 ether; // 100_000_000_000_000_000
+    uint256 constant STARTING_BALANCE = 10 ether;
+
+    // uint256 constant GAS_PRICE = 1; // 1 wei
+
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE); // foundry cheatcode to send money to fake new address
+    }
+
+    function testUserCanFundinteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        fundFundMe.fundFundMe(address(fundMe));
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+
+        assert(address(fundMe).balance == 0);
+    }
+}
