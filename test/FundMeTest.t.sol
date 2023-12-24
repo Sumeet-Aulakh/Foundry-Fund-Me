5af764e9e551283aa439d1b4de3059e9fda90bd9
Sumeet Singh Aulakh <sumeetbinjal@gmail.com>
2023-12-24 06:53:46 -0600

    Add gas usage test and withdraw from multiple funders

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -13,6 +13,8 @@ contract FundMeTest is Test {
     uint256 constant SEND_VALUE = 0.1 ether; // 100_000_000_000_000_000
     uint256 constant STARTING_BALANCE = 10 ether;
 
+    // uint256 constant GAS_PRICE = 1; // 1 wei
+
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
@@ -80,8 +82,16 @@ contract FundMeTest is Test {
         uint256 startingFundMeBalance = address(fundMe).balance;
 
         //Act
+        // // NO GAS SPENT ON LOCAL ANVIL CHAIN, have to use  cheatcodes to set gas price in order to test gas usage.
+        // uint256 gasStart = gasleft();
+        // vm.txGasPrice(GAS_PRICE);
         vm.prank(fundMe.getOwner()); // vm will go over the next vm command
         fundMe.withdraw();
+
+        // uint256 gasEnd = gasleft();
+        // Here tx.gasprice is the current gas price, i.e. GAS_PRICE
+        // uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+        // console.log("Gas used: ", gasUsed);
 
         //Assert
         uint256 endingOwnerBalance = fundMe.getOwner().balance;
@@ -116,6 +126,39 @@ contract FundMeTest is Test {
         // vm.startPrank and vm.stopPrank works same as vm.startBroadcast and vm.stopBroadcast
         vm.startPrank(fundMe.getOwner());
         fundMe.withdraw();
+        vm.stopPrank();
+
+        //Assert
+        assertEq(address(fundMe).balance, 0);
+        assertEq(
+            startingFundMeBalance + startingOwnerBalance,
+            fundMe.getOwner().balance
+        );
+    }
+
+    function testWithdrawFromMultipleFundersCheaper() public funded {
+        /**
+         * Arrange, Act, Assert
+         */
+        //Arrange
+        uint160 numberOfFunders = 10; // Using uint160 because uint160 has same places as in an address
+        uint160 startingNumberOfFunders = 1;
+
+        for (uint160 i = startingNumberOfFunders; i < numberOfFunders; i++) {
+            // vm.prank new address
+            // vm.deal new address
+            // hoax does both things at the same time
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        //Act
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        // vm.startPrank and vm.stopPrank works same as vm.startBroadcast and vm.stopBroadcast
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
         vm.stopPrank();
 
         //Assert
